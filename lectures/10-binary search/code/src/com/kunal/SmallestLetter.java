e6f5623fe1db2d45d55bcc34e677844f7b88b93e
dibyanshu04 <65437781+dibyanshu04@users.noreply.github.com>
2022-02-01 12:20:25 +0530

    Update SmallestLetter.java

--- lectures/10-binary search/code/src/com/kunal/SmallestLetter.java
+++ lectures/10-binary search/code/src/com/kunal/SmallestLetter.java
@@ -10,18 +10,20 @@ public class SmallestLetter {
 
         int start = 0;
         int end = letters.length - 1;
-
-        while(start <= end) {
-            // find the middle element
-//            int mid = (start + end) / 2; // might be possible that (start + end) exceeds the range of int in java
-            int mid = start + (end - start) / 2;
-
-            if (target < letters[mid]) {
-                end = mid - 1;
-            } else {
+        //if target element is greater than the greatest element in the entire array then return element at first index
+        if(target >= letters[end] )
+            return letters[start];
+        
+        while(start<=end){
+            int mid = start + (end-start)/2;
+            
+            if(target < letters[mid]){
+                end = mid-1;
+            }else {
                 start = mid + 1;
             }
         }
-        return letters[start % letters.length];
+//      ans
+        return letters[start];
     }
 }
